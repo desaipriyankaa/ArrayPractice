05dcd3ab8ed77534808977f81f1601d2621b08b5
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-05-13 16:10:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime[] arr = 
+                {
+                    new DateTime ( 2021, 1, 1),
+                    new DateTime ( 2021, 11, 6),
+                    new DateTime ( 2021, 10, 13),
+                    new DateTime ( 2021, 12, 17),
+                    new DateTime ( 2021, 5, 12)
+                };
+            Console.WriteLine("Before insertion :");
+            foreach (var array in arr)
+            {
+                Console.WriteLine(array);
+            }
+
+            Console.WriteLine("\nAfter insertion :");
+            arr[1] = new DateTime(2021, 7, 15);
+            foreach (var array in arr)
+            {
+                Console.WriteLine(array);
+            }
+        }
+    }
+}
